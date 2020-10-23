e884326e0fbc7f7a0ac0dd12dfb342597fe8e8d5
Ekene N <ekenayy@gmail.com>
2020-10-23 03:06:46 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -4,37 +4,33 @@ def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
-  index = 0 
+  counter = 0 
   
-  coupon_cart = cart.each do |cart_item|
-    coupons.each do |coupon_item|
-      if cart_item[:item] === coupon_item[:item] && cart_item[:count] >= coupon_item[:num]
-        new_count =  (cart_item[:count] -= coupon_item[:num])
-        #cart_item[:count] = new_count
-        if coupon_item[:item] + " W/COUPON" === cart_item[:item]
-         coupon_item[:num] += cart_item[:count] 
-          cart_item[count] = new_count
-        else 
-        cart << {:item => cart_item[:item] + " W/COUPON",
-        :price => (coupon_item[:cost] / coupon_item[:num]),
-        :clearance => cart_item[:clearance], :count => coupon_item[:num]}
-        #binding.pry
-        end 
-        #index += 1
-        #coupon_cart
-    #binding.pry
-       end 
-      end
-       #index += 1
-    end 
-      #binding.pry
+  while counter < coupons.length 
+    cart_item = find_item_by_name_in_collection(coupons[counter][:item], cart)
+    couponed_item_name = "#{coupons[counter][:item]} W/COUPON"
+    cart_item_with_coupon = find_item_by_name_in_collection(couponed_item_name, cart)
+    if cart_item && cart_item[:count] >= coupons[counter][:num]
+      if cart_item_with_coupon
+        cart_item_with_coupon[:count] += coupons[counter][:num]
+        cart_item[:count] -= coupons[counter][:num]
+      else 
+        cart_item_with_coupon = {
+          :item => couponed_item_name,
+          :price => coupons[counter][:cost] / coupons[counter][:num], 
+          :count => coupons[counter][:num]
+          :clearance => cart_item[:clearance]
+        }
+        cart << cart_item_with_coupon
+        cart_item[:count] -= coupons[counter][:num]
+      end 
+    end
+    counter += 1
+  end
+  cart
 end
      
 def apply_clearance(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
-  
   clearance_cart = cart.each do |grocery_item|
     #binding.pry 
     if grocery_item[:clearance] == true
@@ -60,7 +56,7 @@ def checkout(cart, coupons)
   consolidated_cart = consolidate_cart(cart)
   after_coupon_cart = apply_coupons(consolidated_cart, coupons)
   clearance_cart = apply_clearance(after_coupon_cart)
-  binding.pry 
+  #binding.pry 
   final_price_array = []
   
   clearance_cart.each do |cart_item|
@@ -73,7 +69,7 @@ def checkout(cart, coupons)
   if final_price > 100
     final_price = (final_price * 0.90).round(2)
   end 
-  binding.pry
+  #binding.pry
   
   final_price 
 end
